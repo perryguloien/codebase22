918e9a62454bb99a595b1fbee35c0150573c527e
Perry Guloien <perryaguloien@gmail.com>
2022-03-30 18:56:43 -0400

    Updating utils.py so print usd not in main

--- app/utils.py
+++ app/utils.py
@@ -16,3 +16,18 @@ def to_usd(my_price):
     """
     return '${:,.2f}'.format(my_price)
 
+
+# nesting code in the main condition will
+# allow other scripts to cleanly import functions from this file
+# without running this code
+
+# this code still gets run when we invoke the script from the command line
+# 
+if __name__ == "__main__":
+# if this code is in the global scope
+# ... of a file we're trying to import from 
+# .... it will throw errors when we try to run those other
+
+    price = input("Please choose a price like 4.99999 ")
+
+    print(to_usd(float(price)))
