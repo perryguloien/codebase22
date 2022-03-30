3f86ae8137040753d05935e7d718dc755ce77f30
Perry Guloien <perryaguloien@gmail.com>
2022-03-30 18:38:33 -0400

    Refactor across files

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+
+
+# this can be blank buthelps files recognize that there are files we want to import from in file directory
---
+++ app/utils.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+def to_usd(my_price):
+    """
+    This is a docstring. It tells us what this function is about.
+    What its responsibilities are. 
+    What the params are about. 
+    What datatypes the params are. 
+    What this fucntion will return.
+    Example of invoking the function.
+
+    Invoke like this: to_usd(9.99999)
+    """
+    return '${:,.2f}'.format(my_price)
+
