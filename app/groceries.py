f83e0e3aa0e6f7438faca3b45657efea1eb6e393
perryguloien <65797833+perryguloien@users.noreply.github.com>
2022-04-05 16:21:03 -0400

    Groceries edits (#7)
    * Refactoring duplicate code
    
    * combining the two loops in groceries.py
    
    * changed median function to mean and added docstring for to_usd function
    
    * Create alphavantage_service.py

--- app/groceries.py
+++ app/groceries.py
@@ -11,13 +11,15 @@ from app.utils import to_usd
 
 
 # checks to see if a products.csv file exists. If not, it uses the default
-if os.path.isfile(os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")) == True:
+user_path = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv") #bringing logic up here
+default_path = os.path.join(os.path.dirname(__file__), "..", "data", "default_products.csv")
+
+if os.path.isfile(user_path) == True:
     print("USING CUSTOM PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+    csv_filepath = user_path
 else:
     print("USING DEFAULT PRODUCTS CSV FILE...")
-    csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "default_products.csv")
-
+    csv_filepath = default_path
 
 
 from pandas import read_csv
@@ -35,16 +37,13 @@ print("---------")
 print("THERE ARE", len(products), "PRODUCTS:")
 print("---------")
 
-for p in products:
-    print("..." + p["name"] + "   " + to_usd(p["price"]))
-
-
 all_prices = []
 for p in products:
+    print("..." + p["name"] + "   " + to_usd(p["price"]))
     all_prices.append(float(p["price"]))
 
 import statistics
-avg_price = statistics.median(all_prices)
+avg_price = statistics.mean(all_prices)
 
 print("---------")
 print("AVERAGE PRICE:", to_usd(avg_price))
